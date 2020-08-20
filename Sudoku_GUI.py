5a50374c72b59e0e463084d37be6750366a8b07b
b-junkoo <46694606+b-junkoo@users.noreply.github.com>
2020-08-20 00:49:04 -0400

    Update Sudoku_GUI.py

--- Sudoku_GUI.py
+++ Sudoku_GUI.py
@@ -8,47 +8,68 @@ pyggame GUI for Sudoku
 """
 import pygame, sys
 from pygame.locals import *
-#from Sudoku_Generator import maker, solution
-#from Sudoku_Solver import solve, check, solver
+from Sudoku_Generator import maker
+from Sudoku_Solver import solve, check, solver
+pygame.font.init()
+
+puzzle, solu = maker()
 
 # Window Size - Width has to be a multiple of 9
 width = 540
 height = 540
 
 # Colors
-color = {'black':[0,0,0], 'white':[255,255,255], 'gray': [200,200,200]}
+color = {'bl':[0,0,0], 'wh':[255,255,255], 'gr': [200,200,200]}
 
-# Draws the grid
-def drawGrid():
-    square = width // 3
-    cell = square // 3
+# Grid and numbers
+square = width // 3
+cell = square // 3
+
+# Draws puzzle numbers 
+def drawNum(display):
+    font = pygame.font.SysFont('comicsans', 60)
+    x = cell // 2
+    y = cell // 2
+    for i in puzzle:
+        for j in i:
+            if j != ' ':      
+                text = font.render(str(j), True, color['bl'])
+                display.blit(text, (x-12,y-15))
+            x += cell
+        x = cell // 2
+        y += cell 
+          
     
+# Draws the grid
+def drawGrid(): 
     # Square lines
     for i in range(0, width, square): # vertical
-        pygame.draw.line(display, color['black'], (i,0),(i,height), 4)
+        if i != 0:
+            pygame.draw.line(display, color['bl'], (i,0),(i,height), 4)
     for j in range(0, height, square): # horizontal
-        pygame.draw.line(display, color['black'], (0,j), (width, j), 4)
+        if j != 0:
+            pygame.draw.line(display, color['bl'], (0,j), (width, j), 4)
     
     # Cell Lines
     for i in range(0, width, cell): # vertical
         if i % square != 0:
-            pygame.draw.line(display, color['black'], (i,0),(i,height), 1)
+            pygame.draw.line(display, color['bl'], (i,0),(i,height), 1)
     for j in range(0, height, cell): # horizontal
         if j % square != 0:
-            pygame.draw.line(display, color['black'], (0,j),(width, j), 1)
+            pygame.draw.line(display, color['bl'], (0,j),(width, j), 1)
     
-    return None
 
 # Window + Title + Main Loop
 def main():
     global display
     
-    pygame.init()
+
     display = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Sudoku")
-    display.fill(color['white'])
+    display.fill(color['wh'])
     
     drawGrid()
+    drawNum(display)
     
     # Main Loop
     while True:
