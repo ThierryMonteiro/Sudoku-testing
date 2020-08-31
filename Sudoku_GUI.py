5af70e7e280c1155c1b3d06f0eef2fbf90c3597d
b-junkoo <46694606+b-junkoo@users.noreply.github.com>
2020-08-31 14:15:08 -0400

    Update Sudoku_GUI.py

--- Sudoku_GUI.py
+++ Sudoku_GUI.py
@@ -19,7 +19,8 @@ width = 540
 height = 540
 
 # Colors
-color = {'bl':[0,0,0], 'wh':[255,255,255], 'gr': [200,200,200], 're':[255,0,0]}
+color = {'bl':[0,0,0], 'wh':[255,255,255], 'gr': [200,200,200], 're':[255,0,0],
+         'input':[0,128,255]}
 
 # Grid and numbers
 square = width // 3
@@ -31,7 +32,12 @@ def drawNum(display):
     y = cell // 2
     for i in puzzle:
         for j in i:
-            if j != ' ':      
+            # inputs are unique in that they're strings which can be used to separate
+            # them from the original numbers
+            if type(j) == str and j != ' ':
+                text = font.render(str(j), True, color['input'])
+                display.blit(text, (x-12,y-15))
+            elif type(j) == int and j != ' ':      
                 text = font.render(str(j), True, color['bl'])
                 display.blit(text, (x-12,y-15))
             x += cell
@@ -57,41 +63,53 @@ def drawGrid():
             pygame.draw.line(display, color['bl'], (0,j),(width, j), 1)
 
 #### Not finished    
-#def highlight(display):
-#    # Cells get highlighted red once selected
-#    pygame.draw.rect(display, color['re'], (x,y, gap ,gap), 3)
+# Returns mouse position
 def click(pos):
     if pos[0] < width and pos[1] < height:
-        x = pos[0] // cell
-        y = pos[1] // cell
-        return (int(x),int(y))
+        col = pos[0] // cell
+        row = pos[1] // cell
+        return (int(row),int(col))
     else:
         return None
+    
+def highlight(pos, display, color, size):
+    # Cells get highlighted red once selected
+    x, y = pos
+    pygame.draw.rect(display, color, (y*cell,x*cell, cell ,cell), size)
+    
 # Fills in number inputs
 def fill_val(display, val, pos):
     x, y = click(pos)
     text = font.render(str(val), 1, color['bl'])
     display.blit(text, (x,y))
+
+# Redraws the board whenever a cell is highlighted to get rid of old highlights
+def redrawer():
+    display.fill(color['wh'])
+    drawGrid()
+    drawNum(display)
     
 # Window + Title + Main Loop
 def main():
     global display
     
-
     display = pygame.display.set_mode((width, height))
+    
     pygame.display.set_caption("Sudoku")
     display.fill(color['wh'])
     key = None
+    run = True
+    select = False
     
     drawGrid()
     drawNum(display)
     
     # Main Loop
-    while True:
-        for event in pygame.event.get():
+    while run:
+        for event in pygame.event.get():         
             if event.type == QUIT:
                 pygame.quit()
-                sys.exit()
+                run = False
             if event.type == KEYDOWN:
                 if event.key == pygame.K_1:
                     key = 1
@@ -112,11 +130,34 @@ def main():
                 if event.key == pygame.K_9:
                     key = 9
             if event.type == MOUSEBUTTONDOWN:
+                select = True
                 pos = pygame.mouse.get_pos()
-                key = None
-                
+                if pos:
+                    redrawer()
+                    highlight(click(pos), display, color['re'], 2)
+                    key = None  
+                    
+        # Takes input and updates puzzle with it so it won't get erased
+        # Input numbers are unique in that they're strings. This is so drawNum
+        # colors them blue instead of black
+        if select == True and key != None:
+            row, col = click(pos)
+            # Only draws numbers if you click on an empty cell
+            if puzzle[row][col] == ' ' or type(puzzle[row][col]) == str:
+                puzzle[row][col] = str(key)
+                # If you're correct, the solution cell also changes to an str
+                # This for an easier comparison later when I add a check solution button
+                if solu[row][col] == key:
+                    solu[row][col] = str(key)
+                redrawer()
+                text = font.render(str(key), True, color['input'])
+                display.blit(text, (col*cell+18, row*cell+15))
+            
+            
+
         pygame.display.update()
 
 
 if __name__ == '__main__':
     main()
+    
